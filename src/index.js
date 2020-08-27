3621ba65074c395bbc33477fba4a80752621eac7
Lucas Duarte <l.duarte.mk@gmail.com>
2020-08-27 02:13:02 -0300

    prettier print on html

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import { cliente1, cliente2, cliente3 } from "./mock/mockClientes"
 
-console.log(cliente1)
-console.log(cliente2)
-console.log(cliente3)
+const allclientes = [cliente1, cliente2, cliente3]
+
+document.querySelector('#main').textContent = 
+JSON.stringify(allclientes, undefined, 2);
+
+
